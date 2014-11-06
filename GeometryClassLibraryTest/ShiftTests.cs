47210dea4a6186791301087775385f2aa42ea1d0
jake_wasdin <jakewclearspaninc@gmail.com>
2014-11-06 09:59:25 -0500

    add negate angle unit test

--- GeometryClassLibraryTest/ShiftTests.cs
+++ GeometryClassLibraryTest/ShiftTests.cs
@@ -1,35 +1,24 @@
 ﻿using NUnit.Framework;
-using GeometryClassLibrary;
-using FluentAssertions;
 using UnitClassLibrary;
-using System.Collections.Generic;
+using FluentAssertions;
 
 namespace GeometryClassLibraryTest
 {
     [TestFixture]
     public class ShiftTests
     {
-        [Test]
-        public void Shift_EqualityTests()
+        [Test()]
+        public void Negate_Test()
         {
-            Rotation rotation1 = new Rotation(Line.ZAxis, new Angle(AngleType.Degree, 45));
-            Rotation rotation2 = new Rotation(Line.ZAxis, new Angle(AngleType.Degree, 45));
+            var originalAngle = new Angle(AngleType.Degree, 45);
+            var oppositeAngle = new Angle(AngleType.Degree, -45);
 
-            Shift testShift1 = new Shift(rotation1, PointGenerator.MakePointWithInches(2, 1, 3));
-            Shift testShift2 = new Shift(rotation2, PointGenerator.MakePointWithInches(2, 1, 3));
+            var negatedOriginalAngle = originalAngle.Negate();
+            var negatedNegatedOriginalAngle = negatedOriginalAngle.Negate();
 
-            testShift1.Equals(testShift2).Should().BeTrue();
+            (originalAngle == negatedNegatedOriginalAngle).Should().BeTrue();
+            (negatedOriginalAngle == oppositeAngle).Should().BeTrue();
 
-            Rotation rotation3 = new Rotation(Line.XAxis, new Angle(AngleType.Degree, 23));
-            Rotation rotation4 = new Rotation(new Line(PointGenerator.MakePointWithInches(0, 2 ,3), PointGenerator.MakePointWithInches(-2,-6,7)), 
-                new Angle(AngleType.Degree, 47));
-            Rotation rotation5 = new Rotation(Line.YAxis, new Angle(AngleType.Degree, -112));
-
-            Shift testShift3 = new Shift(new List<Rotation>() { rotation3, rotation1, rotation4, rotation2, rotation5 }, PointGenerator.MakePointWithInches(-3,4,0));
-            Shift testShift4 = new Shift(new List<Rotation>() { rotation3, rotation1, rotation4, rotation2, rotation5 }, PointGenerator.MakePointWithInches(-3,4,0));
-
-            testShift3.Equals(testShift2).Should().BeFalse();
-            testShift3.Equals(testShift4).Should().BeTrue();
         }
     }
 }
