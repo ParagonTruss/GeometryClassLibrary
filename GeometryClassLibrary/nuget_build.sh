f61d0cf3b6adcb6c8e7bc8d4db5688b6be1ef981
Jake Wasdin <jakewclearspaninc@gmail.com>
2014-12-24 16:42:31 -0500

    fix shell script

--- GeometryClassLibrary/nuget_build.sh
+++ GeometryClassLibrary/nuget_build.sh
@@ -1,5 +1,3 @@
-cd "GeometryClassLibrary"
 rm *.nupkg
 STAMP="$(date +%s)"
 nuget pack GeometryClassLibrary.csproj -Version 1.1.$STAMP
-nuget push *.nupkg -ApiKey b669e128-f9fc-4af8-8594-3b35cb1a11cc
