614636bd7c767348b294184b36cd094e9373e17f
Jake Wasdin <jakewclearspaninc@gmail.com>
2015-01-27 16:32:58 -0500

    fix build server compatibility with assemblyinfo

--- GeometryClassLibrary/Properties/AssemblyInfo.cs
+++ GeometryClassLibrary/Properties/AssemblyInfo.cs
@@ -17,10 +17,10 @@ using System.Runtime.InteropServices;
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+//[assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("166ff822-d022-49f2-8d04-a68f1254f5b5")]
+//[assembly: Guid("166ff822-d022-49f2-8d04-a68f1254f5b5")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,7 +29,6 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyVersionAttribute("1.2.0.0")]
-[assembly: AssemblyFileVersionAttribute("1.2.0.0")]
+[assembly: AssemblyVersion("1.2.*")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: NeutralResourcesLanguageAttribute("en-US")]
