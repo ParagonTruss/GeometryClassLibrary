28af0b6ba7d02f8b45231516b16e78f3a162a042
Charles Taylor <charlestclearspaninc@gmail.com>
2015-06-09 14:07:05 -0400

    Created nuspec file and updated Nuget package for Geometry stub library

--- GeometryStubLibrary/Properties/AssemblyInfo.cs
+++ GeometryStubLibrary/Properties/AssemblyInfo.cs
@@ -1,15 +1,16 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
+using System.Resources;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("GeometryStubLibrary")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A library that does the work of constructing simple geometry objects to cover a wide range of needs.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Clearspan")]
 [assembly: AssemblyProduct("GeometryStubLibrary")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright © Clearspan Components 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +32,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguageAttribute("en-US")]
