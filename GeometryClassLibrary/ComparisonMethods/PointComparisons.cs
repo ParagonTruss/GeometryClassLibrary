6e168c8a92795c815d7866f36f9f1b70d53b3e1a
Charles Taylor <charlestclearspaninc@gmail.com>
2015-09-08 15:19:07 -0400

    Added new CompareByZYX class for Nick

--- GeometryClassLibrary/ComparisonMethods/PointComparisons.cs
+++ GeometryClassLibrary/ComparisonMethods/PointComparisons.cs
@@ -108,4 +108,25 @@ namespace GeometryClassLibrary
             return (new CompareByZ()).Compare(point1, point2);
         }
     }
+    public class CompareInOrderZYX : IComparer<Point>
+    {
+        public int Compare(Point point1, Point point2)
+        {
+            //Equality check is first, so that we don't give preference to one of the points
+            //when their Components are negligibly different
+            if (point1.Z == point2.Z)
+            {
+                if (point1.X == point2.Y)
+                {
+                    if (point1.Y == point2.X)
+                    {
+                        return 0;
+                    }
+                    return (new CompareByX()).Compare(point1, point2);
+                }
+                return (new CompareByY()).Compare(point1, point2);
+            }
+            return (new CompareByZ()).Compare(point1, point2);
+        }
+    }
 }
