1730dd4213ca878e54db45b8f70fe3e4b7791008
Charles Taylor <charlestclearspaninc@gmail.com>
2015-10-22 10:44:28 -0400

    small modifciation

--- GeometryClassLibrary/Coordinates/Point/Point.cs
+++ GeometryClassLibrary/Coordinates/Point/Point.cs
@@ -330,9 +330,9 @@ namespace GeometryClassLibrary
         {
             Vector hypotenuse = new Vector(projectOnto.BasePoint, this);
             Direction lineDirection = projectOnto.Direction;
-            Vector unitVectorAlongLine = new Vector(projectOnto.BasePoint, lineDirection, Distance.Inch);
-            double dotProduct = hypotenuse.DotProduct(unitVectorAlongLine).InchesSquared;
-            return (unitVectorAlongLine * dotProduct).EndPoint;
+           // Vector unitVectorAlongLine = new Vector(projectOnto.BasePoint, lineDirection, Distance.Inch);
+            double dotProduct = hypotenuse.Direction.DotProduct(projectOnto.Direction);
+            return projectOnto.GetPointAlongLine(dotProduct * hypotenuse.Magnitude);
         }
 
         /// <summary>
