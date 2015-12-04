c0a9cb438efaad2c01466013142cdd5dcdd1e531
Charles Taylor <charlestclearspaninc@gmail.com>
2015-12-03 22:17:14 -0500

    initial ideas

---
+++ GeometryClassLibrary/Vectors/IVector.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnitClassLibrary.GenericUnit;
+
+namespace GeometryClassLibrary.Vectors
+{
+    public interface IVector
+    {
+
+    }
+    
+    public interface IVector<T> where T :IUnitType
+    {
+        Unit<T> Magnitude { get; }
+        Direction Direction { get; }
+        IVector<DerivedUnitType> CrossProduct(IVector vector);
+        Unit<DerivedUnitType> DotProduct(IVector vector);
+        
+    }
+
+    public class Vector<T> : IVector where T : IUnitType
+    {
+        public Unit<T> Magnitude { get; private set; }
+        public Direction Direction { get; private set; }
+    }
+
+    public static class IVectorExtensionMethods
+    {
+        public static IVector<DerivedUnitType> CrossProduct(this IVector vector1, IVector vector2)
+        {
+            
+        }
+    }
+}
