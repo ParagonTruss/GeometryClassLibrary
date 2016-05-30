bf5d11f66174de70922a92e30f5593ae4777ab35
Charles Taylor <charlest@paragontruss.com>
2016-05-30 17:40:00 -0400

    Moved extension methods

---
+++ GeometryClassLibrary/ExtensionMethods/IEnumerableExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GeometryClassLibrary
+{
+    public static class IEnumerableExtensions
+    {
+        public static bool ContainsReference<T>(this IEnumerable<T> list, T item)
+        {
+            return list.Any(itemInList => ReferenceEquals(itemInList, item));
+        }
+    }
+}
