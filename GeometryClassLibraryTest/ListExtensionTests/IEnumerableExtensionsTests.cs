30739e9a03b1764db320dee42dd932694bb882f3
Charles Taylor <charlest@paragontruss.com>
2017-05-17 10:47:30 -0400

    Update MoreLinq version

--- GeometryClassLibraryTest/ListExtensionTests/IEnumerableExtensionsTests.cs
+++ GeometryClassLibraryTest/ListExtensionTests/IEnumerableExtensionsTests.cs
@@ -1,9 +1,7 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using FluentAssertions;
 using GeometryClassLibrary;
-using MoreLinq;
 using NUnit.Framework;
 using UnitClassLibrary.DistanceUnit;
 
